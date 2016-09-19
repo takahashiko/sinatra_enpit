e047560c4f403540fe4d19f8121fe8809d9c6af9
Kosei Takahashi <stok20010711@gmail.com>
2016-09-19 06:05:33 +0000

    edit 15:05

--- hello.rb
+++ hello.rb
@@ -1,18 +1,7 @@
 # coding: utf-8
 require 'sinatra'
+require './names'
 
 get '/' do
-<<EOS
-<html>
-  <title>AIIT Hello</title>
-  <body>
-    <h1>'Hello World! AIIT!'</h1>
-    こんにちは！
-  </body>
-</html>
-EOS
-end
-
-get '/hello/:name' do
-  "こんにちは #{params['name']}さん"
+  erb :index
 end
